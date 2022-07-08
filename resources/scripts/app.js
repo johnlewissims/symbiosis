de64c4b42387bf0cbe78da909d784117be8e7a37
John Sims <johnsimsemail@gmail.com>
2022-07-08 16:34:08 -0600

    Strategy Page Updated

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -2,7 +2,7 @@ import {domReady} from '@roots/sage/client';
 import 'jquery';
 import Masonry from 'masonry-layout';
 import 'es-module-shims';
-import './effects/header-2.js';
+//import './effects/header-2.js';
 import Marquee3k from 'marquee3000';
 
 /**
@@ -35,17 +35,35 @@ const main = async (err) => {
 
       // Blog Roll
       var elem = document.querySelector('.blog-row');
-      new Masonry( elem, {
-        gutter: 20,
-        itemSelector: '.blog-tile'
-      });
+      if(elem) {
+        new Masonry( elem, {
+          gutter: 20,
+          itemSelector: '.blog-tile'
+        });
 
+        let startingHeight = $('.blog-list').height();
+        $('.more-row').on( "click", function() {
+          let currentHeight = $('.blog-list').height();
+          $('.blog-list').css("max-height", startingHeight + currentHeight);
+        });
+      }
 
-      let startingHeight = $('.blog-list').height();
-      $('.more-row').on( "click", function() {
-        let currentHeight = $('.blog-list').height();
-        $('.blog-list').css("max-height", startingHeight + currentHeight);
-      });
+      // // Stick Header
+      // var fixed = document.querySelector('.sticky-scroll');
+      // if(fixed) {
+      //   var header = document.querySelector('header');
+      //   document.addEventListener("scroll", function() {
+      //     const el = header;
+      //     let rect = el.getBoundingClientRect();
+      //     console.log(rect.top);
+      //     if (rect.top <= 0 - header.offsetHeight) {
+      //       console.log('here');
+      //       $('.sticky-scroll').addClass('fixed_navbar');
+      //       $('.spacer-section').height(document.querySelector('.sticky-scroll').offsetHeight);
+      //     } else {
+      //     }
+      //   });
+      // }
 
     }
     catch (e) {
--- resources/views/template-our-strategy.blade.php
+++ resources/views/template-our-strategy.blade.php
@@ -9,113 +9,114 @@
 <?php
 $aboutBanner = get_field('about_banner');
 if ($aboutBanner) : ?>
-  <div class="about-banner">
-    <div class="container">
-      <h1><?php echo esc_attr($aboutBanner['banner_text']); ?></h1>
-      <h3><?php echo esc_attr($aboutBanner['sub_text']); ?></h3>
-      <div class="socials">
-        <span>Share</span>
-        <button  onclick="copyUrl()" class="copy">
-          <img src="/wp-content/uploads/2022/05/link.png" alt="Share Link">
-        </button>
-        <a href="#" class="twitter">
-          <img src="/wp-content/uploads/2022/05/Vector.png" alt="Twitter Link">
-        </a>
-        <a href="#" class="instagram">
-          <img src="/wp-content/uploads/2022/05/instagram.png" alt="Instagram Link">
-        </a>
+  <div class="about-1 about-banner sticky-scroll">
+    <div class="container container_2">
+      <h1 class="t-h1 t-green"><?php echo esc_attr($aboutBanner['banner_text']); ?></h1>
+      <?php
+      if ($aboutBanner['image']) {
+      ?>
+        <div class="image_wrapper">
+          <img src="<?php echo $aboutBanner['image']['url']; ?>" alt="<?php echo $aboutBanner['image']['caption']; ?>">
+          <?php if ($aboutBanner['image']['caption']) { ?>
+            <span class="caption t-caption"><?php echo $aboutBanner['image']['caption']; ?></span>
+          <?php } ?>
+        </div>
+      <?php
+      }
+      ?>
+      <div class="row about-banner-2">
+        <div class="col socials">
+          <a href="#" class="twitter">
+            <img src="/wp-content/uploads/2022/05/Vector.png" alt="Twitter Link">
+          </a>
+          <a href="#" class="instagram">
+            <img src="/wp-content/uploads/2022/05/instagram.png" alt="Instagram Link">
+          </a>
+        </div>
+        <div class="col sub_text t-body2 t-green">
+          <?php echo $aboutBanner['sub_text']; ?>
+        </div>
       </div>
     </div>
   </div>
 <?php endif; ?>
 
-<div class="text_section">
-  <div class="container row">
-    <div class="col_1">
-
+<div class="about-2 text_section">
+  <div class="container container_2 row">
       <?php
       $textBlock1 = get_field('text_block_1');
       if ($textBlock1) : ?>
-      <div class="text_block_1">
-        <h3><?php echo esc_attr($textBlock1['header']); ?></h3>
-        <p><?php echo esc_attr($textBlock1['body']); ?></p>
-      </div>
-      <?php endif; ?>
+        <div class="text_block_1">
+          <div class="callout-section callout-section-2 container">
+            <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
+            <h3 class="t-h1 t-green"><?php echo esc_attr($textBlock1['header']); ?></h3>
+          </div>  
+          <?php
+          if ($textBlock1['image']) {
+          ?>
+            <div class="image_wrapper">
+              <img src="<?php echo $textBlock1['image']['url']; ?>" alt="<?php echo $textBlock1['image']['caption']; ?>">
+              <?php if ($textBlock1['image']['caption']) { ?>
+                <span class="caption t-caption"><?php echo $textBlock1['image']['caption']; ?></span>
+              <?php } ?>
+            </div>
+          <?php
+          }
+          ?>  
 
-      <?php
-      $textBlock2 = get_field('text_block_2');
-      if ($textBlock2) : ?>
-      <div class="text_block_2">
-        <h3><?php echo esc_attr($textBlock2['header']); ?></h3>
-        <p><?php echo esc_attr($textBlock2['body']); ?></p>
-      </div>
-      <?php endif; ?>
+          <div class="row">
+            <div class="col_1"></div>
+            <div class="col_2">
+              <?php
+              if ($textBlock1['body']) :
+              ?>
+                <div class="about-2a t-body2 t-green">
+                  <?php echo $textBlock1['body']; ?>
+                </div>    
+              <?php endif; ?>
 
-      <?php
-      $textBlock3 = get_field('text_block_3');
-      if ($textBlock3) : ?>
-      <div class="text_block_3">
-        <h3><?php echo esc_attr($textBlock3['header']); ?></h3>
-        <p><?php echo esc_attr($textBlock3['body']); ?></p>
-      </div>
-      <?php endif; ?>
+              <?php
+              if ($textBlock1['list_item_1']) :
+              ?>
+                <div class="about-2b t-body1 t-green number_box">
+                  <div class="list_item_1">
+                    <span class="number">1</span>
+                    <?php echo $textBlock1['list_item_1']; ?>
+                  </div>
+                </div>    
+              <?php endif; ?>
 
-      <?php
-      $textBlock4 = get_field('text_block_4');
-      if ($textBlock4) : ?>
-      <div class="text_block_4">
-        <h3><?php echo esc_attr($textBlock4['header']); ?></h3>
-        <p><?php echo esc_attr($textBlock4['body']); ?></p>
-      </div>
-      <?php endif; ?>
+              <?php
+              if ($textBlock1['list_item_2']) :
+              ?>
+                <div class="about-2c t-body1 t-green number_box">
+                  <div class="list_item_1">
+                    <span class="number">2</span>
+                    <?php echo $textBlock1['list_item_2']; ?>
+                  </div>
+                </div>
+              <?php endif; ?>
 
-      <?php
-      $launchStatements = get_field('launch_statements');
-      if ($launchStatements) : ?>
-      <div class="launch_statements">
-        <hr>
-        <p><?php echo esc_attr($launchStatements['text']); ?></p>
+              <?php
+              if ($textBlock1['body_2']) :
+              ?>
+                <div class="about-2d t-body2 t-green">
+                  <?php echo $textBlock1['body_2']; ?>
+                </div>
+              <?php endif; ?>
+            </div>
+          </div>
 
-        <div class="button_row">
-          <a class="file_button" href="<?php echo $launchStatements['file_1']['url'] ?>"><?php echo $launchStatements['file_1']['title'] ?></a>
-          <a class="file_button" href="<?php echo $launchStatements['file_2']['url'] ?>"><?php echo $launchStatements['file_2']['title'] ?></a>
-          <a class="file_button" href="<?php echo $launchStatements['file_3']['url'] ?>"><?php echo $launchStatements['file_3']['title'] ?></a>
         </div>
-      </div>
       <?php endif; ?>
-
-    </div>
-
-    <?php
-      $links = get_field('links');
-      if ($links) : ?>    
-      <div class="col_2">
-        <div class="link_box">
-          <p>Links</p>
-          <hr>
-          <a class="link_wrapper" href="<?php echo $links['link_1']['url'] ?>">
-            <img src="/wp-content/uploads/2022/05/arrow.png" alt="arrow">
-            <p><?php echo $links['link_1']['title'] ?></p>
-          </a>
-          <a class="link_wrapper" href="<?php echo $links['link_2']['url'] ?>">
-            <img src="/wp-content/uploads/2022/05/arrow.png" alt="arrow">
-            <p><?php echo $links['link_2']['title'] ?></p>
-          </a>
-          <a class="link_wrapper" href="<?php echo $links['link_3']['url'] ?>">
-            <img src="/wp-content/uploads/2022/05/arrow.png" alt="arrow">
-            <p><?php echo $links['link_3']['title'] ?></p>
-          </a>
-        </div>
-      </div>
-    <?php endif; ?>      
   </div>
 
   <?php
   $callToAction = get_field('call_to_action');
   if ($callToAction) : ?>
-    <div class="container call_to_action">
-      <img src="" alt="background image circle">
-      <h4><?php echo $callToAction['header']; ?></h4>
+    <div class="callout-section callout-section-2 container">
+      <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
+      <h3 class="t-h1 t-green"><?php echo $callToAction['header']; ?></h3>
       <p><?php echo $callToAction['text']; ?></p>
       <a href="<?php echo $callToAction['link']['url']; ?>"><?php echo $callToAction['link']['title']; ?></a>
     </div>
@@ -123,18 +124,15 @@ if ($aboutBanner) : ?>
 
 </div>
 
-  <?php
-    $newTicker = get_field('news_ticker');
-    if ($newTicker) : ?>
-    <div class="marquee3k newsTicker"
-      data-speed="0.25"
-      data-reverse="true"
-      data-pausable="false">
-      <div class="marqueeInner">
-        <?php echo $newTicker; ?>
-      </div>
+<?php
+$newTicker = get_field('news_ticker');
+if ($newTicker) : ?>
+  <div class="marquee3k newsTicker ticker-tape" data-speed="0.25" data-reverse="true" data-pausable="false">
+    <div class="marqueeInner">
+      <?php echo $newTicker; ?>
     </div>
-  <?php endif; ?>
+  </div>
+<?php endif; ?>
 
 <div class="signup-email-form">
   <h2>Subscribe</h2>
