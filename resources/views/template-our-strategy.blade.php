4a6229570e47f7e56ff1a9b12ee0fd41c22a7b56
John Sims <johnsimsemail@gmail.com>
2022-07-08 18:07:13 -0600

    Strategy Template Updates

--- resources/views/template-our-strategy.blade.php
+++ resources/views/template-our-strategy.blade.php
@@ -43,84 +43,171 @@ if ($aboutBanner) : ?>
 
 <div class="about-2 text_section">
   <div class="container container_2 row">
-      <?php
-      $textBlock1 = get_field('text_block_1');
-      if ($textBlock1) : ?>
-        <div class="text_block_1">
-          <div class="callout-section callout-section-2 container">
-            <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
-            <h3 class="t-h1 t-green"><?php echo esc_attr($textBlock1['header']); ?></h3>
-          </div>  
-          <?php
-          if ($textBlock1['image']) {
-          ?>
-            <div class="image_wrapper">
-              <img src="<?php echo $textBlock1['image']['url']; ?>" alt="<?php echo $textBlock1['image']['caption']; ?>">
-              <?php if ($textBlock1['image']['caption']) { ?>
-                <span class="caption t-caption"><?php echo $textBlock1['image']['caption']; ?></span>
-              <?php } ?>
-            </div>
-          <?php
-          }
-          ?>  
-
-          <div class="row">
-            <div class="col_1"></div>
-            <div class="col_2">
-              <?php
-              if ($textBlock1['body']) :
-              ?>
-                <div class="about-2a t-body2 t-green">
-                  <?php echo $textBlock1['body']; ?>
-                </div>    
-              <?php endif; ?>
-
-              <?php
-              if ($textBlock1['list_item_1']) :
-              ?>
-                <div class="about-2b t-body1 t-green number_box">
-                  <div class="list_item_1">
-                    <span class="number">1</span>
-                    <?php echo $textBlock1['list_item_1']; ?>
-                  </div>
-                </div>    
-              <?php endif; ?>
-
-              <?php
-              if ($textBlock1['list_item_2']) :
-              ?>
-                <div class="about-2c t-body1 t-green number_box">
-                  <div class="list_item_1">
-                    <span class="number">2</span>
-                    <?php echo $textBlock1['list_item_2']; ?>
-                  </div>
-                </div>
-              <?php endif; ?>
-
-              <?php
-              if ($textBlock1['body_2']) :
-              ?>
-                <div class="about-2d t-body2 t-green">
-                  <?php echo $textBlock1['body_2']; ?>
-                </div>
-              <?php endif; ?>
-            </div>
-          </div>
-
+    <?php
+    $textBlock1 = get_field('text_block_1');
+    if ($textBlock1) : ?>
+      <div class="text_block_1">
+        <div class="callout-section callout-section-2 container">
+          <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
+          <h3 class="t-h1 t-green"><?php echo esc_attr($textBlock1['header']); ?></h3>
         </div>
-      <?php endif; ?>
+        <?php
+        if ($textBlock1['image']) {
+        ?>
+          <div class="image_wrapper">
+            <img src="<?php echo $textBlock1['image']['url']; ?>" alt="<?php echo $textBlock1['image']['caption']; ?>">
+            <?php if ($textBlock1['image']['caption']) { ?>
+              <span class="caption t-caption"><?php echo $textBlock1['image']['caption']; ?></span>
+            <?php } ?>
+          </div>
+        <?php
+        }
+        ?>
+
+        <div class="row">
+          <div class="col_1"></div>
+          <div class="col_2">
+            <?php
+            if ($textBlock1['body']) :
+            ?>
+              <div class="about-2a t-body2 t-green">
+                <?php echo $textBlock1['body']; ?>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock1['list_item_1']) :
+            ?>
+              <div class="about-2b t-body1 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">1</span>
+                  <?php echo $textBlock1['list_item_1']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock1['list_item_2']) :
+            ?>
+              <div class="about-2c t-body1 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">2</span>
+                  <?php echo $textBlock1['list_item_2']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock1['body_2']) :
+            ?>
+              <div class="about-2d t-body2 t-green">
+                <?php echo $textBlock1['body_2']; ?>
+              </div>
+            <?php endif; ?>
+          </div>
+        </div>
+
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
+
+<div class="about-2 about-3 text_section">
+  <div class="container container_2 row">
+    <?php
+    $textBlock2 = get_field('text_block_2');
+    if ($textBlock2) : ?>
+      <div class="text_block_1">
+        <div class="callout-section callout-section-2 container">
+          <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
+          <h3 class="t-h1 t-green"><?php echo esc_attr($textBlock2['header']); ?></h3>
+        </div>
+        <?php
+        if ($textBlock2['image']) {
+        ?>
+          <div class="image_wrapper">
+            <img src="<?php echo $textBlock2['image']['url']; ?>" alt="<?php echo $textBlock2['image']['caption']; ?>">
+            <?php if ($textBlock2['image']['caption']) { ?>
+              <span class="caption t-caption"><?php echo $textBlock2['image']['caption']; ?></span>
+            <?php } ?>
+          </div>
+        <?php
+        }
+        ?>
+
+        <div class="row">
+          <div class="col_1"></div>
+          <div class="col_2">
+            <?php
+            if ($textBlock2['body']) :
+            ?>
+              <div class="about-2a t-body2 t-green">
+                <?php echo $textBlock2['body']; ?>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock2['list_item_1']) :
+            ?>
+              <div class="about-2b t-body2 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">1</span>
+                  <?php echo $textBlock2['list_item_1']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock2['list_item_2']) :
+            ?>
+              <div class="about-2c t-body2 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">2</span>
+                  <?php echo $textBlock2['list_item_2']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock2['list_item_3']) :
+            ?>
+              <div class="about-2c t-body2 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">3</span>
+                  <?php echo $textBlock2['list_item_3']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+            <?php
+            if ($textBlock2['list_item_4']) :
+            ?>
+              <div class="about-2c t-body2 t-green number_box">
+                <div class="list_item_1">
+                  <span class="number">4</span>
+                  <?php echo $textBlock2['list_item_4']; ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+          </div>
+        </div>
+
+      </div>
+    <?php endif; ?>
   </div>
 
   <?php
   $callToAction = get_field('call_to_action');
   if ($callToAction) : ?>
-    <div class="callout-section callout-section-2 container">
-      <img src="/wp-content/uploads/2022/07/circle_graphic.png" alt="Symbiosis Background Circle Image">
+    <div class="about-4 callout-section callout-section-3 container">
       <h3 class="t-h1 t-green"><?php echo $callToAction['header']; ?></h3>
       <p><?php echo $callToAction['text']; ?></p>
-      <a href="<?php echo $callToAction['link']['url']; ?>"><?php echo $callToAction['link']['title']; ?></a>
+      <a href="<?php echo $callToAction['link']['url']; ?>" class="button bg-green"><?php echo $callToAction['link']['title']; ?></a>
     </div>
   <?php endif; ?>
+
+</div>
 
 </div>
 
