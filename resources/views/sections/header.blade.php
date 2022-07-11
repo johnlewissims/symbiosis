0a0ce25957403369e75ae047fdc7df31f8821050
Brendon Avalos <avalos.brendon@gmail.com>
2022-07-11 15:22:17 -0400

    css style updates
    -css style updates to menu.
    
    -deleted "links" div from older menu.
    
    - added hover effect to sub menu item.
    
    - Need to figure out hover for primary nav.

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary column" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+    <nav class="nav-primary column t-text1" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
     @endif
